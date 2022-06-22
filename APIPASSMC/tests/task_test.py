f7ef465d06a69e4279193f8b388444f2f79b6f71
Roberto Cabrera <rob.cabrerag@duocuc.cl>
2022-06-22 01:30:30 -0400

    testing v1
    con errores

---
+++ APIPASSMC/tests/task_test.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+
+class TaskTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='test', password='12test12', email='test@example.com')
+        self.user.save()
+        self.timestamp = date.today()
+        self.task = Task(user=self.user,
+                         description='description',
+                         due=self.timestamp + timedelta(days=1))
+        self.task.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_read_task(self):
+        self.assertEqual(self.task.user, self.user)
+        self.assertEqual(self.task.description, 'description')
+        self.assertEqual(self.task.due, self.timestamp + timedelta(days=1))
+
+    def test_update_task_description(self):
+        self.task.description = 'new description'
+        self.task.save()
+        self.assertEqual(self.task.description, 'new description')
+
+    def test_update_task_due(self):
+        self.task.due = self.timestamp + timedelta(days=2)
+        self.task.save()
+        self.assertEqual(self.task.due, self.timestamp + timedelta(days=2))
